d85758fa390089fe4af14fe6677cc408f11bd9c4
Aditya Rai <adityarai2312@gmail.com>
2026-05-09 10:47:28 +0530

    updating-client-url

--- src/config/env.js
+++ src/config/env.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const env = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
-  clientUrl: process.env.CLIENT_URL || 'http://localhost:5173',
+  clientUrl: process.env.CLIENT_URL || 'https://three-js-project-frontend-react-js.vercel.app',
   mongoUri: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
